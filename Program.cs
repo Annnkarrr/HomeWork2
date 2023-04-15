0557ed62580fec7ff3f1be0ee2bf568f3d4f0e41
Anastasia <129219450+Annnkarrr@users.noreply.github.com>
2023-04-15 18:05:02 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,77 +1,40 @@
-﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-//645 -> 5
-//78 -> третьей цифры нет
-//32679 -> 6
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
 
-/*Console.WriteLine("Введите число: ");
-int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите цифру, обозначающую день недели: ");
+int day = int.Parse(Console.ReadLine());
 
-if ( number < 100)
+if (day == 1)
 {
-    Console.WriteLine("Число не трехзначное");
+    Console.WriteLine("Понедельник");
 }
+    if ( day == 2)
+    {
+        Console.WriteLine("Вторник");
+    }
+        if (day == 3)
+        {
+            Console.WriteLine("Среда");
+        }
+            if (day == 4)
+            {
+                Console.WriteLine("Четверг");
+            }
+                if (day == 5)
+                {
+                    Console.WriteLine("Пятница");
+                }
+                    if (day == 6)
+                    {
+                        Console.WriteLine("Суббота");
+                    }
+                        if (day == 7)
+                        {
+                            Console.WriteLine("Воскресенье");
+                        }
 else
 {
-    if (number > 999)
-if (number < 100 || number > 999) 
-{
-    Console.WriteLine("Число не трехзначное"); 
-}
-else 
-{
-    number = number % 10;
-    Console.WriteLine($"{number % 10}");
-}
-    {
-        number = number / 10;
-        number = number % 10;
-        Console.WriteLine($"{number % 10}");
-    }
-    else
-    {
-         number = number % 10;
-        Console.WriteLine($"{number % 10}");
-    }
-}
-if (number >= 100)
-{
-    int thirdDigit = {number / 100 % 10};
-    Console.WriteLine($"Третья цифра числа {number} равна {thirdDigit}");
-}
-else
-{
-    Console.WriteLine("Ошибка! Введено двухзначное или меньше число.");
-}*/
-
-int funct(string msg)
-{
-    Console.WriteLine(msg);
-    string value = Console.ReadLine();
-    int result = int.Parse(value);
-    return result;
-}
-
-int thirdDigit(int number)
-{
-    while (number > 999)
-    {
-        number = number/10;
-    }
-    return number % 10;
-}
-
-bool proverka(int number)
-{
-    if (number < 100)
-    {
-        Console.WriteLine("Ошибка ! ! ! Третьей цифры нет");
-        return false;
-    }
-    return true;
-}
-
-int number = funct("Введите число: ");
-if (proverka(number))
-{
-    Console.WriteLine(thirdDigit(number));
+    Console.WriteLine("Такого дня недели не существует");
 }
